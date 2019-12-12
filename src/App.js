a7e1bb1d544ba448915375b3feaf6d9e29874692
Eric Suescun <edsuescun@gmail.com>
2019-12-12 16:59:52 -0500

    Se implementa la busqueda en linea sobre los objetos.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,36 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { searchTerm: '' }
+  }
+
+  handleChange(e) {
+    this.setState({ searchTerm: e.target.value })
+    console.log(this.state.searchTerm);
+  }
+
   render() {
+    let items = posts;
+    if(this.state.searchTerm) {
+      items = items.filter(item => 
+        item.title.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+      )
+    }
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" value={this.state.searchTerm} onChange={this.handleChange.bind(this)} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {items.map((post, index) => 
+            <li key={'id' + index}>
+              <a href={post.url}><img src={post.image } alt={post.title} /></a>
+              <p>{ post.title }</p>
+            </li>
+          )}
         </ul>
       </div>
     )
